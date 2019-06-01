3532da09bf9b9f7a29fb91676454444d9b197004
tony trinh <tony.w.trinh3@gmail.com>
2019-06-01 16:01:26 -0700

    'sidebar'

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,27 +1,19 @@
 import React from 'react';
 
 
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import './NavBar.css';
 
 function NavBar(){
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-    function handleClick(event) {
-      setAnchorEl(event.currentTarget);
-    }
   
-    function handleClose() {
-      setAnchorEl(null);
-    }
   
     return (
       <div className = "Nav-Bar">
         <img src = "https://www.clickbank.com/wp-content/uploads/2017/11/ClickBank_Logo_WEB-7.png" alt="logo"/>
         <div className = 'item'>
             <h3>Affiliate MarketPlace</h3>
+            <h3>Support</h3>
+            <h3>Create Account</h3>
+            <h3>Login</h3>
         </div>
 
       </div>
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import './SideBar.css';
 
 function SideBar(){
+
+        
+
+
     return(
         <div>
-            Side Bar
+            <header className = 'side-bar-header'>Side Bar</header>
+
+            <nav className = 'side-bar'>
+                    <a className = "collapsible">Art & Entertainment</a>
+                        <a className = "content">Art</a>
+                        <a className = "content">Body Art</a>
+                    <a className = "collapsible">Business/Investing</a>
+                    <a className = "collapsible">Computers/Internet</a>
+                    <a className = "collapsible">E-Business & E-Marketing</a>
+                    <a className = "collapsible">Education</a>
+            </nav>
+
         </div>
     )
 };
 
 export default SideBar;
+
+{/* <a>Employment & Jobs</a>
+<a>Fiction</a>
+<a>Food, Wine & Cooking</a>
+<a>Games</a>
+<a>Green Products</a>
+<a>Health & Fitness</a>
+<a>Home & Garden</a>
+<a>Languages</a>
+<a>Mobile</a>
+<a>Parenting & Famiaes</a>
+<a>Poatics/Current Events</a>
+<a>References</a>
+<a>Self Help</a>
+<a>Software & Services</a>
+<a>Spirituaaty, New Age & Alternative Beaefs</a>
+<a>Sports</a>
+<a>Travel</a> */}
